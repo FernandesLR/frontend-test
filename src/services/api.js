293d187d63d84fb72f21f6188d742bb665556f7b
FernandesLr <leo1ramosf@gmail.com>
2025-03-02 12:03:56 -0300

    feat: add api responsável por receber e fazer chamadas para api externa

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import md5 from 'js-md5';
+
+const API_URL = "https://gateway.marvel.com/v1/public/characters";
+const API_PUBLIC_KEY = import.meta.env.VITE_API_PUBLIC_KEY;
+const API_PRIVATE_KEY = import.meta.env.VITE_API_PRIVATE_KEY;
+
+
+// Função para gerar o hash MD5
+const gerarHash = () => {
+    const ts = Date.now().toString(); // Timestamp único como string
+    const hash = md5(ts + API_PRIVATE_KEY + API_PUBLIC_KEY);  // Concatenando como string
+    return { ts, hash };
+};
+
+// Função para buscar personagens
+export const getCharacters = async (limit = 20, offset = 0) => {
+    try {
+        const { ts, hash } = gerarHash();
+        const response = await fetch(`${API_URL}?ts=${ts}&apikey=${API_PUBLIC_KEY}&hash=${hash}&limit=${limit}&offset=${offset}`);
+        
+        if (!response.ok) {
+            throw new Error("Erro ao buscar dados da API");
+        }
+
+        const data = await response.json();
+        //console.log(data.data)
+        return data.data.results;
+    } catch (error) {
+        console.error('Erro ao buscar dados da API:', error.message || error);
+        return [];  // Retorna uma lista vazia caso haja erro
+    }
+};
+
+export default { getCharacters, gerarHash };
