28ab64770be3e3c9d6edcd9d443a3cd602341ef2
FernandesLr <leo1ramosf@gmail.com>
2025-03-02 19:56:32 -0300

    feat: implementa a lógica filtrar pelo campo de busca

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,38 @@
-import logo from '../assets/logo/Group.png'
-import searchIcon from '../assets/busca/Lupa/shape.png'
+import { useState, useCallback } from 'react';
+import logo from '../assets/logo/Group.png';
+import searchIcon from '../assets/busca/Lupa/shape.png';
 
-function Header(mostrarDescricao = true) {  // Padrão: true (evita precisar definir sempre)
+function Header({ mostrarDescricao = true, className, setSearchTerm }) {
+  const [searchValue, setSearchValue] = useState('');
+
+  // Função para lidar com a mudança do campo de busca
+  const handleSearchChange = useCallback((event) => {
+    const value = event.target.value;
+    setSearchValue(value);
+    setSearchTerm(value);  // Passa o valor da busca para o componente pai
+  }, [setSearchTerm]);
+
   return (
-        <header>
-            <img src={logo} alt="Marvel logo" className='logo' />
-            <h1>EXPLORE O UNIVERSO</h1>
-            {
-                mostrarDescricao ?
-                    <p>Mergulhe no domínio deslumbrante de todos 
-                    os personagens clássicos que você ama - e aqueles
-                    que você descobrirá em breve!
-                    </p> 
-                : ''
-            }
-    
-            <div className="search">
-              <img src={searchIcon} alt="" />
-              <input type="text" placeholder="Procure por heróis" />
-            </div>
-        </header>
-  )
+    <header className={className}>
+      <img src={logo} alt="Marvel logo" className="logo" />
+      <h1>EXPLORE O UNIVERSO</h1>
+      {mostrarDescricao && (
+        <p>
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - 
+          e aqueles que você descobrirá em breve!
+        </p>
+      )}
+      <div className="search">
+        <img src={searchIcon} alt="Ícone de busca" />
+        <input 
+          type="text" 
+          placeholder="Procure por heróis"
+          value={searchValue}
+          onChange={handleSearchChange}  // Atualiza o valor da busca
+        />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
