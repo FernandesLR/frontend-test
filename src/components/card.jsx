3c1fba2f823a25697e1ed829a09593c66f51b62a
FernandesLr <leo1ramosf@gmail.com>
2025-03-03 10:27:48 -0300

    feat: add Link que leva para a página HeroPage

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import heart from '../assets/icones/heart/heart.svg'; // Ícone de coração cheio
 import heart2 from '../assets/icones/heart/Path.svg'; // Ícone de coração vazio
 import './card.css'; // Estilos para o Modal
+import { Link } from 'react-router-dom';
 
 function Card({ id, name, thumbnail, favoritos, setFavoritos }) {
     const [showModal, setShowModal] = useState(false);
@@ -40,29 +41,34 @@ function Card({ id, name, thumbnail, favoritos, setFavoritos }) {
     const isFavorito = favoritos.some(hero => hero.id === id); // Verificação se está nos favoritos
 
     return (
-        <div className={`card ${disableHover ? 'disable-hover' : ''}`}>
-            <img 
-                src={`${thumbnail.path}.${thumbnail.extension}`} 
-                alt={name} 
-                className="card-image"
-            />
-            <div className="card-header">
-                <h3>{name}</h3>
-                <button id="favoritar" onClick={handleFavorite}>
-                    <img src={isFavorito ? heart2 : heart} alt="Favoritar" />
-                </button>
-            </div>
+            <div className={`card ${disableHover ? 'disable-hover' : ''}`}>
+            <Link to={`/hero-page/${id}`}>
+                    <img 
+                        src={`${thumbnail.path}.${thumbnail.extension}`} 
+                        alt={name} 
+                        className="card-image"
+                    />
+            </Link>
+                <div className="card-header">
+                    <h3>{name}</h3>
+                    <button id="favoritar" onClick={handleFavorite}>
+                        <img src={isFavorito ? heart2 : heart} alt="Favoritar" />
+                    </button>
 
-            {/* Modal de Limite de Favoritos */}
-            {showModal && (
-                <div className="modal" onClick={handleOverlayClick}>
-                    <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
-                        <h2>Limite de Favoritos Atingido</h2>
-                        <p>Você já pode favoritar no máximo 5 heróis!</p>
-                    </div>
                 </div>
-            )}
+
+                {/* ------------------------------ */}
+                {/* Modal de Limite de Favoritos */}
+                {showModal && (
+                    <div className="modal" onClick={handleOverlayClick}>
+                        <div className="modal-content">
+                            <span className="close" onClick={closeModal}>&times;</span>
+                            <h2>Limite de Favoritos Atingido</h2>
+                            <p>Você já pode favoritar no máximo 5 heróis!</p>
+                        </div>
+                    </div>
+                )}
+        
         </div>
     );
 }
